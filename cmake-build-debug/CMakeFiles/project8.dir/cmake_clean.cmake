492de4bb14f5210d636468f19cf4fb0e77cb2d96
HanchuShen <Hanchu.shen@vanderbilt.edu>
2024-06-15 13:58:05 -0700

    commit

---
+++ cmake-build-debug/CMakeFiles/project8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../project8.exe"
+  "../project8.exe.manifest"
+  "../project8.pdb"
+  "CMakeFiles/project8.dir/WordCount.cpp.obj"
+  "libproject8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
